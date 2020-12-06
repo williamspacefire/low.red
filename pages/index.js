d941a5bbee4196778518349b8024785d11b26f7f
William Spacefire <william@meycup.com>
2020-12-06 14:51:39 -0300

    Some work

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
-import { AppBar, Container, Grid, Paper, Toolbar, Typography } from "@material-ui/core";
+import { Button, Container, Grid, IconButton, InputBase, Paper, Typography } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
 import Image from 'next/image';
+import Header from '../components/header.js';
+
+
 
 export default function index() {
     return (
         <>
-            <AppBar position="fixed">
-                <Toolbar>
-                    <Typography>LOW.RED</Typography>
-                </Toolbar>
-            </AppBar>
-            <Toolbar/>
+            <Header title="LOW.RED - Simple Url Shortener"/>
             <Container>
             <br/><br/><br/><br/><br/><br/>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        <Grid container justify="left" spacing={10}>
+                        <Grid container justify="left" spacing={4}>
                             <Grid key="1" item>
                                 <Typography variant="h2">
                                     Short links, big results
@@ -24,11 +23,27 @@ export default function index() {
                                 </Typography>
                             </Grid>
                             <Grid key="2" item>
-                                <Image src="/home.jpg" width={480} height={381}/>
+                                <Image priority={true} src="/home.jpg" width={480} height={381}/>
                             </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
+                <Paper component="form" style={{padding:"5px",marginTop:"10px",display:"flex"}}>
+                    <InputBase 
+                        name="short"
+                        style={{padding:"4px",flex:1,}} 
+                        placeholder="Short a url"/>
+                        <IconButton>
+                            <Search/>
+                        </IconButton>
+                </Paper>
+                <Paper style={{padding:"20px", display:"flex",marginTop:10}} elevation={2}>
+                    <Typography style={{flex:1}} variant="body1">https://blog.meycup.com/asdpasd0afasf-asfasf-asfas-fasf</Typography>
+                    <Typography variant="body1">https://low.red/O9od</Typography>
+                    <Button color="primary">
+                        Copy
+                    </Button>
+                </Paper>
             </Container>
         </>
     )
