08e99b9c4045ca604595e14d073f979b946ef0e4
William Spacefire <william@meycup.com>
2020-12-10 10:17:33 -0300

    Bug fix

--- pages/api/v1/short/[url].js
+++ pages/api/v1/short/[url].js
@@ -1,8 +1,21 @@
 import { host, user, database, password } from '../../../../components/env';
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: host,
+    user: user,
+    password: password,
+    database: database
+});
 
 export default function handler(req, res) {
+
+    const { query: {url}, } = req;
+    let newUrl = url;
+
+    while(newUrl.includes("%2f")) newUrl =  newUrl.replace("%2f", "-")
+
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ host: host }))
+    res.end(JSON.stringify({ deus: newUrl }))
   }
   
